5c06d7dd486b5fa2dcdb1d639c2e6c51b96fbe39
Mukta Aphale <AMukta@gmail.com>
2014-11-20 11:44:22 +0530

    Test program

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,49 @@
+package recfun
+import common._
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+
+    @tailrec
+    def loop(open: Int, rest: List[Char]): Int = {
+      if (open < 0 || rest.isEmpty)
+        open
+      else if (rest.head == '(')
+        loop(open + 1, rest.tail)
+      else if (rest.head == ')')
+        loop(open - 1, rest.tail)
+      else
+        loop(open, rest.tail)
+    }
+
+    loop(0, chars) == 0
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (coins.isEmpty || money < 0) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
+}
